4d17d229fc5814e5ebc796fc579206fafb8a4582
Jack Nelson <jack@jacknelson.xyz>
2021-09-04 16:57:05 -0400

    added proper puzzle finding

--- main.py
+++ main.py
@@ -21,19 +21,25 @@ from chia.types.spend_bundle import SpendBundle
 from chia.pools.pool_wallet import PoolSingletonState
 from chia.pools.pool_wallet_info import PoolState
 
+# define constants + config
+config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+overrides = config["network_overrides"]["constants"][config["selected_network"]]
+constants: ConsensusConstants = DEFAULT_CONSTANTS.replace_str_to_bytes(**overrides)
+
 
 class CoinTools:
-    def __init__(self, config: Dict, constants: ConsensusConstants):
+    def __init__(self, config: Dict, constants: ConsensusConstants, coin_id: Optional[bytes32] = None):
         # start logging
         self.log = logging
-        self.log.basicConfig(level=logging.INFO)
+        self.log.basicConfig(level=logging.DEBUG)
         # load chia config and constants
         self.config = config
         self.constants = constants
         self.self_hostname = config["self_hostname"]
+        self.coin_id = coin_id
         # define tasks and clients
         self.node_rpc_client: Optional[FullNodeRpcClient] = None
-        self.search_coin_task: Optional[asyncio.Task] = None
+        self.main_coin_task: Optional[asyncio.Task] = None
 
     # start rpc node connection
     async def start(self):
@@ -41,36 +47,36 @@ class CoinTools:
             self.self_hostname, uint16(8555), DEFAULT_ROOT_PATH, self.config
         )
         self.log.info("Connected to node at %s:%d", self.self_hostname, 8555)
-        self.search_coin_task = asyncio.create_task(self.search_coin())
+        # self.main_coin_task = asyncio.create_task(self.main_coin())
+        await self.main_coin()
+        await self.stop()
 
     async def stop(self):
-        if self.search_coin_task is not None:
-            self.search_coin_task.cancel()
+        if self.main_coin_task is not None:
+            self.main_coin_task.cancel()
         self.node_rpc_client.close()
         await self.node_rpc_client.await_closed()
 
-    async def get_coin_info(self, coin_id: bytes32) -> Optional[CoinRecord]:
-        coin_info: Optional[CoinRecord] = await self.node_rpc_client.get_coin_record_by_name(coin_id)
-        if coin_info is None:
+    async def get_coin_info(self, coin_id: bytes32) -> Optional[CoinSpend]:
+        base_coin_info: Optional[CoinRecord] = await self.node_rpc_client.get_coin_record_by_name(coin_id)
+        if base_coin_info is None:
             self.log.warning(f"Can not find genesis coin {coin_id}")
             return None
-        if not coin_info.spent:
+        if not base_coin_info.spent:
             self.log.warning(f"Genesis coin {coin_id} not spent")
             return None
+        coin_info: Optional[CoinSpend] = \
+            await self.node_rpc_client.get_puzzle_and_solution(self.coin_id, base_coin_info.spent_block_index)
         return coin_info
 
-    async def search_coin(self):
-        while True:
-            coin_id: bytes32 = hexstr_to_bytes(input(f"Enter launcher id: "))
-            self.log.info(coin_id.hex())
-            coin_info = await self.get_coin_info(coin_id)
-            print(coin_info)
+    async def main_coin(self):
+        if self.coin_id is None:
+            self.coin_id: bytes32 = hexstr_to_bytes(input(f"Enter launcher id: "))
+        result = await self.get_coin_info(self.coin_id)
+        self.log.info(result)
 
 
-config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-overrides = config["network_overrides"]["constants"][config["selected_network"]]
-constants: ConsensusConstants = DEFAULT_CONSTANTS.replace_str_to_bytes(**overrides)
-tool_server = CoinTools(config, constants)
+tool_server: CoinTools = CoinTools(config, constants)
 
 
 async def start_all():
